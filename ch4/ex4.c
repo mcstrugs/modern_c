bf4eb4e62dcf5127c9b54ad9334583d7982a572b
mcstrugs <joe@joemartinson.org>
2023-03-01 16:55:53 -0800

    ex 4 i said so

---
+++ ch4/ex4.c
@@ -0,0 +1,13 @@
+// ex 4
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("In octal, your number is: %o\n", num);
+
+    return 0;
+}
